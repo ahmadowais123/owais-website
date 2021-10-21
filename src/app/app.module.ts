666635bea351848491209ec4a98c641480d287ba
Owais Khan <owais.khan2@mail.mcgill.ca>
2021-10-21 12:48:35 -0400

    Add about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about-page', component: AboutPageComponent}
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
